c4029bbb1d956a3197ff015989c13b067079bfe5
Munachi <muachiukeme@gmail.com>
2026-06-15 13:59:49 +0100

    update cors

--- VendorBackend/server.js
+++ VendorBackend/server.js
@@ -29,8 +29,9 @@ app.use(helmet());
 
 app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "admin-key", 'x-session-id'],
+  allowedHeaders: ["Content-Type", "Authorization","x-session-id", "admin-key",],
 }));
 
 // 3. General rate limiter - applies to all routes
