a84ee7374fb592d1271f3df91c656d969c9e3500
Munachi <muachiukeme@gmail.com>
2026-07-15 13:04:42 +0100

    fixed authcontext export error

--- VendorFrontend/src/pages/SignupPage.jsx
+++ VendorFrontend/src/pages/SignupPage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 import { registerSeller, getBanks, verifyAccount } from "../api/api";
 import styles from "./SignupPage.module.css";
 
